a4054689f3b6ad14d2a2ee8f45a70c1147628608
Arawindor <arawindor@yahoo.com>
2021-01-30 18:49:23 +0100

    new_django.sh regex and final push

--- new_django.sh
+++ new_django.sh
@@ -9,10 +9,10 @@ echo ""
 mkdir -p $project_path
 cd $project_path
 pwd
-echo "New project folder created at $project_path"
+echo -e e "\e[91mNew project folder created at $project_path\e[0m"
 
 # Git init oif the new project folder
-git config --global init.defaultBranch main
+#git config --global init.defaultBranch main
 git init 
 
 # Insert standard .gitignore file
@@ -21,22 +21,27 @@ echo ".pyc
 /*_venv/" > .gitignore
 echo -e "\e[91mCreated standard .gitignore\e[0m"
 echo ""
+echo "# Django startproject automator
+
+Arawindor's Django create project has set up local paht, Git, GitHub connection, Python Venv and Django installation." > README.md
 git add .
 git commit -m "First commit"
 echo ""
 
 # Create a brand new GitHub.com repository
-echo -e "\e[91m---> Create a new repository at www.github.com and call it $projectname\e[0m"
+echo -e "\e[91m---> Create a new repository at www.github.com and call it $project_name\e[0m"
 echo ""
 
 # Setup the github.com repository
-echo -e "\e[91mSetting the url of 'origin' repository\e[0m"
+echo -e "\e[91mSetup the origin repository\e[0m"
 echo ""
 echo -e "\e[41m   Type the GitHub.com repository link:\e[0m"
 read github_repository_link
 github=$github_repository_link
+github_user_repo=$(echo $github | grep -oP '(?<=github.com/).*?(?=.git)')
 git remote add origin $github
 git remote -v
+echo $github_user_repo
 
 # Setup the SSH/GPG keys
 echo -e "\e[91mGitHub login and SSH/GPG keys\e[0m"
@@ -44,7 +49,7 @@ echo ""
 while true; do
     read -p "Do you have a SSH key associated with your github.com account?	[yes/no] " yn
     case $yn in
-        [Yy]* ) echo -e "\e[41m   Enter the GitHub 'username/repositoryname' of the repository, mind that is case sensitive and you must enter the '/' also! (eg: for https://github.com/Arawindor/create_project.git type 'Arawindor/create_project')\e[0m";read github_user_repo;git remote set-url origin git@github.com:$github_user_repo.git;break;;
+        [Yy]* ) git remote set-url origin git@github.com:$github_user_repo.git;break;;
         [Nn]* ) echo -e "\e[41m   Enter your GitHub username and password below\e[0m";break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -65,11 +70,16 @@ echo -e "\e[91mPython Venv and Django\e[0m"
 while true; do
     read -p "Do you wish to setup Python Venv and install Django into the new venv? [yes/no] " yn
     case $yn in
-        [Yy]* ) python3 -m venv django_venv;source django_venv/bin/activate;pip install django;deactivate;break;;
+        [Yy]* ) python3 -m venv django_venv;source django_venv/bin/activate;pip install django;django-admin startproject $project_name;deactivate;break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+# Final setup commit
+git add .
+git commit -m "Django startproject"
+git push
 
 # Exit message
 echo ""
